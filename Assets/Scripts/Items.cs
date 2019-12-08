992bef75c625df8789db2cdc9a61b895de193624
Michal Magdon <machoninio@gmail.com>
2019-12-08 16:23:04 +0100

    Add Items script

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum eIteamsType
+{
+    none,
+    eCoin,
+    eGoldBar,
+    eLife
+}
+
+
+public class Items : MonoBehaviour
+{
+    [Header("Definiowane w panelu inspekcyjnym")]
+    public eIteamsType type = eIteamsType.none;
+    public float lifeTime = 10;
+
+    [Header("Definiowane dynamicznie")]
+    public float bornTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bornTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bornTime + lifeTime < Time.time)
+            Destroy(this.gameObject);
+    }
+}
