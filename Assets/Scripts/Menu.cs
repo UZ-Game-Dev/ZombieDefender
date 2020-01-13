277471cb97f96e3687a3c76b9537d307bc7c7c41
Przemek Woźny <przemwo123@gmail.com>
2020-01-13 19:35:44 +0100

    Stworzenie ekranu menu i śmierci. Dodano TextMeshPro
    Stworzone zostały dwie nowe sceny - scena menu i tymczasowa śmierci, nowy skrypt menu obsługujący przyciski w powyższych scenach. Dodano tymczasową teksturę przycisku. Dodano rozszerzenia TextMeshPro.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void ButtonNewGame()
+    {
+        SceneManager.LoadScene("_MainScene");
+    }
+
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+
+    public void ButtonBackToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
