21eca6ff767ccf18ee3bec879c85dc837e769ddf
Przemwo <przemwo123@gmail.com>
2020-03-18 18:02:57 +0100

    Dodano menu zapytania o potwierdzenie czy wyjść z gry

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -11,6 +11,11 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
+    //dodanie UI zapytania o potwierdzenie czy wyjść z gry
+    public GameObject infoExitUI;
+    public GameObject blackBackground;
+    //
+
     void Awake()
     {
         if (S != null)
@@ -34,22 +39,40 @@ public class PauseMenu : MonoBehaviour
     private void ActivateMenu()
     {
         Time.timeScale = 0;
-        //AudioListener.pause = true;
+        //AudioListener.pause = true; <- linijka do usunięcia
         pauseMenuUI.SetActive(true);
     }
 
     public void DeactivateMenu()
     {
         Time.timeScale = 1;
-        //AudioListener.pause = false;
+        //AudioListener.pause = false; <- linijka do usunięcia
+        // Zamyka okno zapytania po wciśnięciu Esc
+        infoExitUI.SetActive(false);
         pauseMenuUI.SetActive(false);
         _isPaused = false;
     }
 
     public void ButtonBackToMenu()
     {
+        //Time.timeScale = 1f; < -linijka do usunięcia
+        //SceneManager.LoadScene("MenuScene"); < -linijka do usunięcia
+        //zmiana, teraz otwiera się menu zapytania o potwierdzenie czy wyjść z gry
+        infoExitUI.SetActive(true);
+        //
+    }
+
+    //dodanie przycisków menu zapytania o potwierdzenie czy wyjść z gry
+    public void ButtonYes()
+    {
+        blackBackground.SetActive(true);
         Time.timeScale = 1f;
         SceneManager.LoadScene("MenuScene");
     }
 
+    public void ButtonNo()
+    {
+        infoExitUI.SetActive(false);
+    }
+    //
 }
